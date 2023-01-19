077646cb6cee5fd76e151757dfe176d3ce56c287
강수림 <eathergs012@gmail.com>
2023-01-19 12:34:01 +0900

    build success

---
+++ imbling/src/main/java/com/imbling/ImblingApplication.java
@@ -0,0 +1,13 @@
+package com.imbling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImblingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImblingApplication.class, args);
+	}
+
+}
---
+++ imbling/src/main/java/com/imbling/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.imbling.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping(path = { "/", "/home" })
+	public String home() {
+		
+		return "home";
+	}
+	
+
+}
+
