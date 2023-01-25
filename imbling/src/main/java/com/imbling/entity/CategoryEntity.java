bb36f38a34cc03f587d86ac2fc0299372e8672c5
hanjihae <wlgo32@naver.com>
2023-01-25 18:03:55 +0900

    product

---
+++ imbling/src/main/java/com/imbling/entity/CategoryEntity.java
@@ -0,0 +1,40 @@
+package com.imbling.entity;
+
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="imb_category")
+public class CategoryEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(nullable=false)
+	private int categoryNo;			// 카테고리 식별자 
+	
+	@Column
+	private String categoryName;	// 카테고리 이름 
+	
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "categoryNo")
+	private Collection<ProductEntity> products;
+}
