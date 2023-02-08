4d15b057590fe1f5719682a4e189f69542caa748
kiju <kiju2912@naver.com>
2023-02-08 10:15:43 +0900

    orderlist

--- imbling/src/main/java/com/imbling/controller/OrderController.java
+++ imbling/src/main/java/com/imbling/controller/OrderController.java
@@ -1,15 +1,36 @@
 package com.imbling.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.imbling.dto.OrderDto;
+import com.imbling.service.UserOrderService;
 
 @Controller @RequestMapping(path="/admin")
 public class OrderController {
 	
+
+    @Autowired
+    @Qualifier("userOrderService")
+    private UserOrderService userOrderService;
+	
+	
 	@GetMapping(path= {"order-list"})
-	public String showAdminOrderList() {
+	public String showAdminOrderList(Model model) {
+		List<OrderDto> orders=userOrderService.findAll();
+		for(OrderDto order:orders ) {
+			System.out.println(order);
+
+		}
 		
+		model.addAttribute("orders", orders);
+
 		return "admin/order/order-list";
 	}
 
